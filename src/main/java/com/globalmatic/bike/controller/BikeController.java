efb4cbcd9c0365acab2748d98275641f63d6a6f2
unknown <kudrethairet@gmail.com>
2020-05-14 06:38:13 -0700

    adding delete and Edit UI

--- src/main/java/com/globalmatic/bike/controller/BikeController.java
+++ src/main/java/com/globalmatic/bike/controller/BikeController.java
@@ -3,6 +3,7 @@ package com.globalmatic.bike.controller;
 
 import com.globalmatic.bike.models.Bike;
 import com.globalmatic.bike.repositories.BikeRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -34,6 +35,19 @@ public class BikeController {
     @GetMapping("/{id}")
     public Bike get(@PathVariable("id") long id){
         return bikeRepository.getOne(id);
+    }
+
+
+    @RequestMapping(value="{id}", method = RequestMethod.PUT)
+    public Bike update(@PathVariable Long id, @RequestBody Bike bike ){
+        Bike existBike = bikeRepository.getOne(id);
+        BeanUtils.copyProperties(bike, existBike, "id");
+        return bikeRepository.saveAndFlush(bike);
+    }
+
+    @RequestMapping(value="{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable long id){
+        bikeRepository.deleteById(id);
     }
 
 }
